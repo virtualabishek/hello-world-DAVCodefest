720b630607ccf78f221f42d8c53027db1550b885
Arun Khanal <zarunkhanal1@gmail.com>
2025-07-25 15:45:25 +0545

    added
    solving ai openkey issue

--- backend/src/DB/db.js
+++ backend/src/DB/db.js
@@ -1,12 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const DBCONNECT=async()=>{
-    try {
-        const db=await mongoose.connect(`${process.env.DB_URI}/${process.env.DB_NAME}`)
-        console.log(db.connection.host)
-        console.log("db connected")
-
-    } catch (error) {
-        throw new Error("DB not connected",error)
-    }
-}
+export const DBCONNECT = async () => {
+  try {
+    const db = await mongoose.connect(
+      `${process.env.DB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(db.connection.host);
+    console.log("db connected");
+  } catch (error) {
+    throw new Error("DB not connected", error);
+  }
+};
+// this is test
