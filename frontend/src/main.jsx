b6d154717a6a99ec16099037b12e87758d588da8
Arun Khanal <zarunkhanal1@gmail.com>
2025-07-25 10:31:21 +0545

    initialized homepage and landing page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import LandingPage from "./pages/LandingPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <div>This is our Landing Page</div>;
+};
+
+export default LandingPage;
